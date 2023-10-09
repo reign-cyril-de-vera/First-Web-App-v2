a99e03817d5c118309c5d764b96752a7cc5ff412
Reign Cyril De Vera <reigncyyyril@gmail.com>
2023-10-09 11:03:03 +0800

    feat: add database

--- create_todos.py
+++ create_todos.py
@@ -1,11 +1,15 @@
 from app import app, db, Todo
 
 # Create an application context
-app.app_context().push()
+# app.app_context().push()
 
 # first_todo = Todo(todo_text="Learn Flask")
 # db.session.add(first_todo)
 # db.session.commit()
 
-all_todos = Todo.query.all()
-print(all_todos[0].todo_text)
+# all_todos = Todo.query.all()
+# print(all_todos[0].todo_text)
+
+# Make database tables
+# with app.app_context():
+#     db.create_all()
