e973968328e82b6a3c19f560439687eef4952341
Reign Cyril De Vera <reigncyyyril@gmail.com>
2023-10-09 12:12:06 +0800

    feat: edit design

--- create_todos.py
+++ create_todos.py
@@ -3,10 +3,12 @@ from app import app, db, Todo
 # Create an application context
 # app.app_context().push()
 
+# Create a new todo using python script
 # first_todo = Todo(todo_text="Learn Flask")
 # db.session.add(first_todo)
 # db.session.commit()
 
+# View all todos using python script
 # all_todos = Todo.query.all()
 # print(all_todos[0].todo_text)
 
